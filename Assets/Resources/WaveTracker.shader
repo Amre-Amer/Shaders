4f3b61537eb0cfaae6849e2a2bd01b90fd51d0c6
Amre <me@amre-amer.com>
2018-06-10 12:01:32 -0700

    audio visualization

---
+++ Assets/Resources/WaveTracker.shader
@@ -0,0 +1,104 @@
+﻿Shader "Custom/WaveTracker" {
+    Properties {
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+//        _Param1 ("Wave Params 1", Vector) = (6.0, 2.0, 0.12, 0.0)
+//        _Param2 ("Wave Params 2", Vector) = (12.0, 10.0, 0.03, 0.0)
+        _Size ("Size", Float) = 50
+        _NumCenters ("NumCenters", Int) = 100
+        _RangeRed ("RangeRed", Float) = 2
+        _RangeGreen ("RangeRed", Float) = 3
+        _RangeBlue ("RangeRed", Float) = 5
+    }
+    SubShader {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+        LOD 200
+        Cull Off
+        
+        CGPROGRAM
+        #pragma surface surf Lambert alpha vertex:vert
+
+        sampler2D _MainTex;
+        half4 _Param1;
+        half4 _Param2;
+        float _Size;
+        int _NumCenters;
+        float3 _Centers[100];
+        float _RangeRed;
+        float _RangeGreen;
+        float _RangeBlue;
+
+        void vert (inout appdata_full v) {
+            half time = _Time.y;
+            //half u = v.texcoord.x;
+            //half w1 = sin(time * _Param1.x - u * _Param1.y) * _Param1.z;
+            //half w2 = sin(time * _Param2.x - u * _Param2.y) * _Param2.z;
+            //v.vertex.xyz += v.normal * (w1 + w2) * u;
+            //v.vertex.xyz += v.normal * _Radius;
+        }
+
+        struct Input {
+            float2 uv_MainTex;
+            float3 worldPos;
+        };
+
+        void surf (Input IN, inout SurfaceOutput o) {
+//            o.Albedo = tex2D (_MainTex, IN.uv_MainTex).rgb;
+
+//            half time = _Time.y;
+//            float rad = _Radius / 2 + _Radius / 10 * sin(time);
+//            float dN = 1 - saturate(d / rad);
+            float sumC = 0;
+            float sumDist = 0;
+            for (int n = 0; n < _NumCenters; n++) {
+                float size = _Size;
+                float offset = 1;
+                float dist = distance(_Centers[n], IN.worldPos);
+                float s = 1;
+                if (n == 1) {
+                    s = 2;
+                }
+                if (n == 2) {
+                    s = 3;
+                }
+                if (n == 3) {
+                    s = 5;
+                }
+                float c = sin(dist * size * s);
+                c = (c / 2) + 0.5;
+                if (c > 0.85) {
+                   c = 1;
+                } else {
+                   c = 0;
+                }
+                sumC += c;
+                sumDist += dist; 
+            }
+            float cAve = sumC / _NumCenters;
+            //
+            float r = 0;
+            float g = 0;
+            float b = 0;
+            float aveDist = sumDist / _NumCenters;
+            if (aveDist < _RangeRed) {
+                r = 0.75 - cAve;
+                g = 0;
+                b = 0;
+            }
+            if (aveDist > _RangeRed && aveDist < _RangeGreen) {
+                r = 0;
+                g = 0.75 - cAve;
+                b = 0;
+            }
+            if (aveDist > _RangeGreen && aveDist < _RangeBlue) {
+                r = 0;
+                g = 0;
+                b = 0.75 - cAve;
+            }
+            o.Albedo = half3(cAve + r, cAve + g, cAve + b);
+            o.Alpha = 1;
+        }
+        ENDCG
+    } 
+    FallBack "Diffuse"
+    CustomEditor "WavyAnimatedMaterialInspector"
+}
---
+++ Assets/Zones/WaveTracker.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveTracker : MonoBehaviour {
+	int numGos = 3;
+	[Range(0f, .1f)]
+    public float speed = .05f;
+	[Range(0, 100)]
+	public float size = 21;
+	[Space]
+	[Header("Ave Distance Between")]
+    [Range(0, 8)]
+    public float rangeRed = 2;
+    [Range(0, 8)]
+    public float rangeGreen = 3;
+    [Range(0, 8)]
+    public float rangeBlue = 5;
+	GameObject[] gos;
+	GameObject goFloor;
+	Material material;
+	Vector3[] targets;
+	Vector3[] looks;
+	float smooth = .95f;
+	float distNear = 2;
+	GameObject[] targetGos;
+	GameObject[] lookGos;
+	int numFLoorClones = 100;
+	float spacingFloors = .1f;
+	// Use this for initialization
+	void Start () {
+		CreateFloor();
+		CreateGos();
+		//CreateFloorClones();
+		Debug.Log("gos:" + gos.Length + "\n");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateTargets();
+		UpdateAvoids();
+		UpdateTargetGos();
+		UpdateLookGos();
+		UpdateGos();
+		UpdateShader();		
+	}
+
+	void CreateGos() {
+		gos = new GameObject[numGos];
+		for (int n = 0; n < numGos; n++) {
+			gos[n] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			gos[n].transform.position = GetRandomPos();
+		}        		
+	}
+	void CreateFloor() {
+		goFloor = GameObject.CreatePrimitive(PrimitiveType.Quad);
+		goFloor.transform.eulerAngles = new Vector3(90, 0, 0);
+		goFloor.transform.localScale = new Vector3(10, 10, 10);
+		goFloor.GetComponent<Renderer>().material = new Material(Shader.Find("Custom/WaveTracker"));
+	}
+	void UpdateShader()
+    {
+		Vector4[] gos4 = new Vector4[numGos];
+		material = goFloor.GetComponent<Renderer>().material;
+		for (int n = 0; n < numGos; n++)
+		{
+			Vector3 p = gos[n].transform.position;
+			gos4[n] = new Vector4(p.x, p.y, p.z, 0);
+		}
+		material.SetVectorArray("_Centers", gos4);
+		material.SetInt("_NumCenters", numGos);
+		material.SetFloat("_Size", size);
+		material.SetFloat("_RangeRed", rangeRed);
+		material.SetFloat("_RangeGreen", rangeGreen);
+		material.SetFloat("_RangeBlue", rangeBlue);
+    }
+	void CreateFloorClones() {
+		for (int n = 1; n < numFLoorClones / 2; n++)
+        {
+			float x = goFloor.transform.position.x;
+			float yOffset = (float) n * spacingFloors;
+			float y = goFloor.transform.position.y;
+			float z = goFloor.transform.position.z;
+            //
+			GameObject go1 = Instantiate(goFloor);
+			go1.transform.position = new Vector3(x, y + yOffset, z);
+			//
+            GameObject go2 = Instantiate(goFloor);
+            go2.transform.position = new Vector3(x, y - yOffset, z);
+        }
+	}
+	void UpdateTargetGos() {
+		if (targetGos == null)
+        {
+			targetGos = new GameObject[numGos];
+        }
+		for (int n = 0; n < numGos; n++)
+        {
+			if (targetGos[n] == null) {
+				targetGos[n] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				targetGos[n].transform.localScale = new Vector3(.25f, .25f, .25f);
+				targetGos[n].GetComponent<Renderer>().material.color = Color.blue;
+			}
+			targetGos[n].transform.position = targets[n];
+        }
+	}
+	void UpdateLookGos() {
+		if (lookGos == null)
+        {
+			lookGos = new GameObject[numGos];
+        }
+		for (int n = 0; n < numGos; n++)
+        {
+            if (lookGos[n] == null)
+            {
+                lookGos[n] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                lookGos[n].transform.localScale = new Vector3(.25f, .25f, .25f);
+				lookGos[n].GetComponent<Renderer>().material.color = Color.cyan;
+            }
+			lookGos[n].transform.position = looks[n];
+        }
+	}
+	void UpdateGos() {
+		for (int n = 0; n < numGos; n++)
+		{
+			looks[n] = smooth * looks[n] + (1 - smooth) * targets[n];
+			GameObject go = gos[n];
+			go.transform.LookAt(looks[n]);
+			go.transform.eulerAngles = new Vector3(0, go.transform.eulerAngles.y, 0);
+			go.transform.position += go.transform.forward * speed;
+			go.transform.position = new Vector3(go.transform.position.x, 0, go.transform.position.z);
+		}
+	}
+	void UpdateTargets() {
+		if (targets == null) {
+			targets = new Vector3[numGos];
+			looks = new Vector3[numGos];
+		}
+		for (int n = 0; n < numGos; n++) {
+			float dist = Vector3.Distance(gos[n].transform.position, targets[n]);
+			if (dist < distNear) {
+				targets[n] = GetRandomPos();
+			}
+		}
+	}
+	void UpdateAvoids() {
+		for (int n = 0; n < numGos; n++)
+		{
+			CheckAvoid(n);
+		}
+	}
+	void CheckAvoid(int n) {
+		int nNearest = GetNearest(n);
+		if (nNearest >= 0)
+		{
+			float dist = Vector3.Distance(gos[n].transform.position, gos[nNearest].transform.position);
+			if (dist < distNear)
+			{
+				Vector3 vector = gos[n].transform.position - gos[nNearest].transform.position;
+				targets[n] = gos[n].transform.position + vector * distNear * 1;
+			}
+		}
+	}
+	int GetNearest(int nCheck) {
+		int nNearest = -1;
+		float distNearest = -1;
+		for (int n = 0; n < numGos; n++) {
+			if (n != nCheck) {
+				float dist = Vector3.Distance(gos[nCheck].transform.position, gos[n].transform.position);
+				if (nNearest == -1 || dist < distNearest) {
+					distNearest = dist;
+					nNearest = n;
+				}
+			}
+		}
+		return nNearest;
+	}
+	Vector3 GetRandomPos() {
+		float sizeX = goFloor.transform.localScale.x / 2;
+        float sizeY = goFloor.transform.localScale.y / 2;
+        float sizeZ = goFloor.transform.localScale.z / 2;
+		float x = goFloor.transform.position.x + Random.Range(-sizeX, sizeX);
+        float y = .5f;
+        float z = goFloor.transform.position.z + Random.Range(-sizeZ, sizeZ);
+		return new Vector3(x, y, z);
+	}
+}
