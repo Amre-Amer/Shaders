05d87779975e0bdc1a511e1ac651c1554d73a5e4
Amre <me@amre-amer.com>
2018-06-11 13:49:03 -0700

    SoundRoom

---
+++ Assets/Resources/SoundRoom.shader
@@ -0,0 +1,65 @@
+﻿Shader "Custom/SoundRoom" {
+    Properties {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+        _Size ("Size", Float) = 50
+        _Thickness ("Thickness", Float) = 0.15
+        _Amplitude ("Amplitude", Float) = 0.5
+    }
+    SubShader {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+        LOD 200
+        Cull Off
+        
+        CGPROGRAM
+        #pragma surface surf Lambert alpha vertex:vert
+
+        fixed4 _Color;
+        sampler2D _MainTex;
+        float _Size;
+        float3 _Center;
+        float _Thickness;
+        float _Amplitude;
+
+        void vert (inout appdata_full v) {
+            half time = _Time.y;
+            half u = v.texcoord.x;
+        }
+
+        struct Input {
+            float2 uv_MainTex;
+            float3 worldPos;
+        };
+
+        void surf (Input IN, inout SurfaceOutput o) {
+            fixed4 cTex4 = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            half3 cTex = cTex4.rgb;
+            //
+                float dist = distance(_Center, IN.worldPos);
+                float amp = _Amplitude;
+                //_Size = 1;
+                float ang = dist * amp / _Size; //dist; // * amp / _Size;
+                float v = sin(ang);
+                v = (v / 2) + 0.5;  // saturate?
+                if (v > (1 - _Thickness)) {
+                   v = 1;
+                } else {
+                   v = 0;
+                }
+            //
+            half3 cValue  = half3(v, v, v);
+            //
+            float cAve = (cValue + cTex) / 2;
+            half3 cResult = cAve;
+            //cResult = cValue;
+//            if (sumValue == 1 || _Thickness >= 0.5) {
+//                cResult = (cTex + cRange) / 2;
+//            }
+            o.Albedo = cResult;
+            o.Alpha = 1;
+        }
+        ENDCG
+    } 
+    FallBack "Diffuse"
+    CustomEditor "WavyAnimatedMaterialInspector"
+}
---
+++ Assets/SoundRoom/SoundRoom.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRoom : MonoBehaviour {
+    public Material material;
+    [Range(0f, 10f)]
+    public float size = .2f;
+    [Range(0f, 1f)]
+    public float thickness = .2f;
+    float[] amplitudes;
+    int numCenters = 1;
+    GameObject centerGo;
+    AudioSource audioSource;
+    float min = -1;
+    float max = -1;
+    int cntFrames;
+    float amplitude;
+    // Use this for initialization
+	void Start () {
+        centerGo = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        amplitudes = new float[numCenters];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateAmplitude();
+        //UpdateCenter();
+        UpdateMaterial();
+        cntFrames++;
+	}
+    void UpdateMaterial() {
+        Vector3 pos = centerGo.transform.position;
+        Vector4 pos4 = new Vector4(pos.x, pos.y, pos.z, 0);
+        material.SetVector("_Center", pos4);
+        material.SetFloat("_Size", size);
+        material.SetFloat("_Thickness", thickness);
+//        amplitude = Random.Range(0f, 1f);
+        material.SetFloat("_Amplitude", amplitude);
+    }
+    void UpdateAmplitude()
+    {
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+            audioSource.loop = true;
+            while (!(Microphone.GetPosition(null) > 0)) { }
+            audioSource.Play();
+        }
+        float[] sample = new float[1];
+        audioSource.GetOutputData(sample, 0);
+        float value = sample[0];
+        if (min == -1 || value < min)
+        {
+            min = value;
+        }
+        if (max == -1 || value > max)
+        {
+            max = value;
+        }
+        //float amplitude = 0;
+        float range = max - min;
+        if (range > 0)
+        {
+            amplitude = (value - min) / range;
+        }
+    }
+    void UpdateCenter() {
+        float rad = 3;
+        float x = rad * Mathf.Cos(cntFrames * Mathf.Deg2Rad);
+        float y = 0;
+        float z = rad * Mathf.Sin(cntFrames * Mathf.Deg2Rad);
+        centerGo.transform.position = new Vector3(x, y, z);
+        float s = .25f + amplitude * 3;
+        centerGo.transform.localScale = new Vector3(s, s, s);
+    }
+}
